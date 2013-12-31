9e74fe2c2f218808dd9a7e7916732b671825c62e
Viktor <trustno1>
2013-12-31 01:50:29 +0100

    Added lesson: Control Structures Added answers

--- src/test/java/com/wordpress/technologyconversations/learning/solutions/Lesson01BasicsTest.scala
+++ src/test/java/com/wordpress/technologyconversations/learning/solutions/Lesson01BasicsTest.scala
@@ -1,11 +1,11 @@
-package com.wordpress.technologyconversations.learning
+package com.wordpress.technologyconversations.learning.solutions
 
 import com.wordpress.technologyconversations.learning.specs.UnitSpec
 
 class Lesson01BasicsTest extends UnitSpec {
 
   "Value" should "be declared with val" in {
-    val expected = ()
+    val expected = 35
     val myValue = 35
     assertResult(expected) {
       myValue
@@ -13,7 +13,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   it should "be constant and its content cannot be changed" in {
-    val expected = ()
+    val expected = 35
     val myValue = 35
     // Following line, if not commented, would not compile
     // myValue = 5
@@ -23,7 +23,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   it can "have the type inferred from the type of the expression or it can be specified" in {
-    val expected = ()
+    val expected = "Hello"
     val myValue: String = "Hello" // This is a string
     val myOtherValue = "Hello" // This is also a string
     assertResult(myValue) {
@@ -35,7 +35,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Multiple values" can "be declared together" in {
-    val expected = ()
+    val expected = 42
     val myValue1, myValue2 = 42 // Both values have the same content 42
     assertResult(myValue1) {
       myValue2
@@ -46,7 +46,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Variable" should "be declared with var" in {
-    val expected = ()
+    val expected = "How are you?"
     var myVariable = "How are you?"
     assertResult(expected) {
       myVariable
@@ -54,7 +54,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   it can "change its content" in {
-    val expected = ()
+    val expected = "Fine, thanks for asking"
     var myVariable = "How are you?"
     myVariable = "Fine, thanks for asking"
     assertResult(expected) {
@@ -63,7 +63,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Numeric type" can "be Byte, Char, Short, Int, Long, Float and Double. They are all classses and can invoke methods" in {
-    val expected = ()
+    val expected = 42
     val myStringValue = 42.toString
     assertResult("42") {
       myStringValue
@@ -75,14 +75,14 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Arithmetic operators" should "work in the same was as in Java or C++" in {
-    val expected = ()
+    val expected = 20
     val myValue = (1 + 3) * 5
     assertResult(expected) {
       myValue
     }
   }
   it should "be methods" in {
-    val expected = ()
+    val expected = 3
     val myValue1 = 1 + 2
     val myValue2 = 1.+(2) // Both ways of using methods are allowed
     assertResult(myValue1) {
@@ -94,7 +94,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Functions and methods" should "be supported" in {
-    val expected = ()
+    val expected = 16
     val myValue = scala.math.pow(2, 4) // Functions, imports and math package will be explained later. For now, pow raises first parameter to the power of second.
     assertResult(expected) {
       myValue
@@ -102,7 +102,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Method" should "be used without parenthesis when it doesn't have parameters" in {
-    val expected = ()
+    val expected = "abc"
     val myValue = "aaabbc".distinct // Distinct will be explained later
     assertResult(expected) {
       myValue
@@ -110,7 +110,7 @@ class Lesson01BasicsTest extends UnitSpec {
   }
 
   "Method apply" can "be omitted" in {
-    val expected = ()
+    val expected = 'h'
     val myValue1 = "Technology"(3) // Apply depends on the type. For strings, apply returns character at specified position.
     val myValue2 = "Technology".apply(3) // Both are the same.
     assertResult(myValue1) {
