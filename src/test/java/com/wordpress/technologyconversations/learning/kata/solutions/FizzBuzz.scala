4245a5df703a21c751b02dbd9c9ff3b9c5073b01
vfarcic <viktor.farcic@gtech.com>
2014-01-10 10:32:20 +0100

    Classes lesson refactor

--- src/test/java/com/wordpress/technologyconversations/learning/kata/solutions/FizzBuzz.scala
+++ src/test/java/com/wordpress/technologyconversations/learning/kata/solutions/FizzBuzz.scala
@@ -4,12 +4,19 @@ import com.wordpress.technologyconversations.learning.specs.UnitSpec
 
 object FizzBuzz {
 
-  def getResult(number: Int) = {
-    var result = ""
-    if (number % 3 == 0) result = "fizz"
-    if (number % 5 == 0) result += "buzz"
-    if (result == "") number.toString
-    else result
+//  def getResult(number: Int) = {
+//    var result = ""
+//    if (number % 3 == 0) result = "fizz"
+//    if (number % 5 == 0) result += "buzz"
+//    if (result == "") number.toString
+//    else result
+//  }
+
+  def getResult(number: Int): String = (number % 3, number % 5) match {
+    case (0 , 0) => "fizzbuzz"
+    case (0 , _) => "fizz"
+    case (_ , 0) => "buzz"
+    case  _      => number.toString()
   }
 
 }
