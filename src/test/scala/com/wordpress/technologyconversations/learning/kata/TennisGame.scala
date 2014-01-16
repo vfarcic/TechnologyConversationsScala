3f77a0560dfd7f06c76903dc4d7b9e8939b96713
Viktor <trustno1>
2014-01-17 00:19:24 +0100

    Published

--- src/test/scala/com/wordpress/technologyconversations/learning/kata/TennisGame.scala
+++ src/test/scala/com/wordpress/technologyconversations/learning/kata/TennisGame.scala
@@ -2,14 +2,18 @@ package com.wordpress.technologyconversations.learning.kata
 
 import org.scalatest.{GivenWhenThen, FeatureSpec, Matchers, FlatSpec}
 
-class TennisGame(player1: String, player2: String) {
+class TennisGame(player1: Player, player2: Player) {
 
-  val points = collection.mutable.Map[String, Int]()
+  def score = { }
 
-  def winBall(player: String) { }
+}
 
-  def score() = { }
-
+class Player(val name: String) {
+  var score = 0
+  def winBall { }
+}
+object Player {
+  def apply(name: String) = {new Player(name)}
 }
 
 // http://codingdojo.org/cgi-bin/wiki.pl?KataTennis
@@ -21,66 +25,82 @@ class TennisGame(player1: String, player2: String) {
 class TennisGameUnitTest extends FlatSpec with Matchers {
 
   "Points" can "be added to each player" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Victor")
-    game.winBall("Victor")
-    game.winBall("Sarah")
-    game.winBall("Victor")
-    game.points("Victor") should be (3)
-    game.points("Sarah") should be (1)
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    victor.winBall
+    victor.winBall
+    sarah.winBall
+    victor.winBall
+    victor.score should be (3)
+    sarah.score should be (1)
   }
 
   "Love" should "be description for score 0" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.score() should be ("love, love")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    game.score should be ("love, love")
   }
 
   "Fifteen" should "be description for score 1" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Sarah")
-    game.score() should be ("love, fifteen")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    sarah.winBall
+    game.score should be ("love, fifteen")
   }
 
   "Thirty" should "be description for score 2" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Victor")
-    game.winBall("Victor")
-    game.winBall("Sarah")
-    game.score() should be ("thirty, fifteen")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    victor.winBall
+    victor.winBall
+    sarah.winBall
+    game.score should be ("thirty, fifteen")
   }
 
   "Forty" should "be description for score 3" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    game.score() should be ("forty, love")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    game.score should be ("forty, love")
   }
 
   "Advantage" should "describe when least three points have been scored by each side and a player has one more point than his opponent" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    (1 to 4).foreach(x => game.winBall("Sarah"))
-    game.score() should be ("advantage Sarah")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    (1 to 4).foreach(x => sarah.winBall)
+    game.score should be ("advantage Sarah")
   }
 
   "Deuce" should "be description when at least three points have been scored by each player and the scores are equal" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    (1 to 3).foreach(x => game.winBall("Sarah"))
-    game.score() should be ("deuce")
-    game.winBall("Victor")
-    game.score() should not be ("deuce")
-    game.winBall("Sarah")
-    game.score() should be ("deuce")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    (1 to 3).foreach(x => sarah.winBall)
+    game.score should be ("deuce")
+    victor.winBall
+    game.score should not be "deuce"
+    sarah.winBall
+    game.score should be ("deuce")
   }
 
   "Game" should "be won by the first player to have won at least four points in total and with at least two points more than the opponent" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 4).foreach(x => game.winBall("Victor"))
-    (1 to 3).foreach(x => game.winBall("Sarah"))
-    game.score() should not be ("Victor won")
-    game.score() should not be ("Sarah won")
-    game.winBall("Victor")
-    game.score() should be ("Victor won")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 4).foreach(x => victor.winBall)
+    (1 to 3).foreach(x => sarah.winBall)
+    game.score should not be "Victor won"
+    game.score should not be "Sarah won"
+    victor.winBall
+    game.score should be ("Victor won")
   }
 
 }
@@ -90,70 +110,72 @@ class TennisGameScenarioTest extends FeatureSpec with GivenWhenThen with Matcher
   scenario("Simple tennis game") {
 
     Given("new game between Victor and Sarah starts")
-    val game = new TennisGame("Victor", "Sarah")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
 
     Then("score is love, love")
-    game.score() should be ("love, love")
+    game.score should be ("love, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is fifteen, love")
-    game.score() should be ("fifteen, love")
+    game.score should be ("fifteen, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is thirty, love")
-    game.score() should be ("thirty, love")
+    game.score should be ("thirty, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is forty, love")
-    game.score() should be ("forty, love")
+    game.score should be ("forty, love")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is forty, fifteen")
-    game.score() should be ("forty, fifteen")
+    game.score should be ("forty, fifteen")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is forty, thirty")
-    game.score() should be ("forty, thirty")
+    game.score should be ("forty, thirty")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is deuce")
-    game.score() should be ("deuce")
+    game.score should be ("deuce")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is advantage Victor")
-    game.score() should be ("advantage Victor")
+    game.score should be ("advantage Victor")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is deuce")
-    game.score() should be ("deuce")
+    game.score should be ("deuce")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is advantage Sarah")
-    game.score() should be ("advantage Sarah")
+    game.score should be ("advantage Sarah")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is Sarah won")
-    game.score() should be ("Sarah won")
+    game.score should be ("Sarah won")
 
   }
 
--- src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/TennisGame.scala
+++ src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/TennisGame.scala
@@ -1,24 +1,31 @@
 package com.wordpress.technologyconversations.learning.kata.solutions
 
 import org.scalatest.{GivenWhenThen, FeatureSpec, Matchers, FlatSpec}
+import scala.util.Sorting
 
-class TennisGame(player1: String, player2: String) {
+class TennisGame(player1: Player, player2: Player) {
 
-  val points = collection.mutable.Map(player1 -> 0, player2 -> 0)
-  val pointsDescription = Array("love", "fifteen", "thirty", "forty")
-
-  def winBall(player: String) { points(player) += 1 }
-
-  def score() = {
-    val arr = points.toList.sortBy(_._2)
-    val (lp, wp) = (arr(0)._1, arr(1)._1) // Loosing and winning players
-    if (points(wp) >= 3 && points(lp) >= 3) {
-      if (points(wp) - points(lp) >= 2) wp + " won"
-      else if (points(lp) == points(wp)) "deuce"
-      else "advantage " + wp
-    } else pointsDescription(points(player1)) + ", " + pointsDescription(points(player2))
+  def score = {
+    if (player1.score >= 3 && player2.score >= 3) {
+      if (Math.abs(player2.score - player1.score) >= 2) leadPlayer.name + " won"
+      else if (player1.score == player2.score) "deuce"
+      else "advantage " + leadPlayer.name
+    } else player1.scoreDescription + ", " + player2.scoreDescription
   }
 
+  def leadPlayer = if (player1 > player2) player1 else player2
+
+}
+
+class Player(val name: String) extends Ordered[Player] {
+  val pointsDescription = Array("love", "fifteen", "thirty", "forty")
+  var score = 0
+  def scoreDescription = pointsDescription(score)
+  def compare(that: Player) = this.score - that.score
+  def winBall { score += 1 }
+}
+object Player {
+  def apply(name: String) = {new Player(name)}
 }
 
 /*
@@ -34,66 +41,82 @@ Implement a simple tennis game
 class TennisGameUnitTest extends FlatSpec with Matchers {
 
   "Points" can "be added to each player" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Victor")
-    game.winBall("Victor")
-    game.winBall("Sarah")
-    game.winBall("Victor")
-    game.points("Victor") should be (3)
-    game.points("Sarah") should be (1)
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    victor.winBall
+    victor.winBall
+    sarah.winBall
+    victor.winBall
+    victor.score should be (3)
+    sarah.score should be (1)
   }
 
   "Love" should "be description for score 0" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.score() should be ("love, love")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    game.score should be ("love, love")
   }
 
   "Fifteen" should "be description for score 1" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Sarah")
-    game.score() should be ("love, fifteen")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    sarah.winBall
+    game.score should be ("love, fifteen")
   }
 
   "Thirty" should "be description for score 2" in {
-    val game = new TennisGame("Victor", "Sarah")
-    game.winBall("Victor")
-    game.winBall("Victor")
-    game.winBall("Sarah")
-    game.score() should be ("thirty, fifteen")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    victor.winBall
+    victor.winBall
+    sarah.winBall
+    game.score should be ("thirty, fifteen")
   }
 
   "Forty" should "be description for score 3" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    game.score() should be ("forty, love")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    game.score should be ("forty, love")
   }
 
   "Advantage" should "describe when least three points have been scored by each side and a player has one more point than his opponent" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    (1 to 4).foreach(x => game.winBall("Sarah"))
-    game.score() should be ("advantage Sarah")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    (1 to 4).foreach(x => sarah.winBall)
+    game.score should be ("advantage Sarah")
   }
 
   "Deuce" should "be description when at least three points have been scored by each player and the scores are equal" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 3).foreach(x => game.winBall("Victor"))
-    (1 to 3).foreach(x => game.winBall("Sarah"))
-    game.score() should be ("deuce")
-    game.winBall("Victor")
-    game.score() should not be "deuce"
-    game.winBall("Sarah")
-    game.score() should be ("deuce")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 3).foreach(x => victor.winBall)
+    (1 to 3).foreach(x => sarah.winBall)
+    game.score should be ("deuce")
+    victor.winBall
+    game.score should not be "deuce"
+    sarah.winBall
+    game.score should be ("deuce")
   }
 
   "Game" should "be won by the first player to have won at least four points in total and with at least two points more than the opponent" in {
-    val game = new TennisGame("Victor", "Sarah")
-    (1 to 4).foreach(x => game.winBall("Victor"))
-    (1 to 3).foreach(x => game.winBall("Sarah"))
-    game.score() should not be "Victor won"
-    game.score() should not be "Sarah won"
-    game.winBall("Victor")
-    game.score() should be ("Victor won")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
+    (1 to 4).foreach(x => victor.winBall)
+    (1 to 3).foreach(x => sarah.winBall)
+    game.score should not be "Victor won"
+    game.score should not be "Sarah won"
+    victor.winBall
+    game.score should be ("Victor won")
   }
 
 }
@@ -103,70 +126,72 @@ class TennisGameScenarioTest extends FeatureSpec with GivenWhenThen with Matcher
   scenario("Simple tennis game") {
 
     Given("new game between Victor and Sarah starts")
-    val game = new TennisGame("Victor", "Sarah")
+    val victor = Player("Victor")
+    val sarah = Player("Sarah")
+    val game = new TennisGame(victor, sarah)
 
     Then("score is love, love")
-    game.score() should be ("love, love")
+    game.score should be ("love, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is fifteen, love")
-    game.score() should be ("fifteen, love")
+    game.score should be ("fifteen, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is thirty, love")
-    game.score() should be ("thirty, love")
+    game.score should be ("thirty, love")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is forty, love")
-    game.score() should be ("forty, love")
+    game.score should be ("forty, love")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is forty, fifteen")
-    game.score() should be ("forty, fifteen")
+    game.score should be ("forty, fifteen")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is forty, thirty")
-    game.score() should be ("forty, thirty")
+    game.score should be ("forty, thirty")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is deuce")
-    game.score() should be ("deuce")
+    game.score should be ("deuce")
 
     When("Victor wins the ball")
-    game.winBall("Victor")
+    victor.winBall
 
     Then("score is advantage Victor")
-    game.score() should be ("advantage Victor")
+    game.score should be ("advantage Victor")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is deuce")
-    game.score() should be ("deuce")
+    game.score should be ("deuce")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is advantage Sarah")
-    game.score() should be ("advantage Sarah")
+    game.score should be ("advantage Sarah")
 
     When("Sarah wins the ball")
-    game.winBall("Sarah")
+    sarah.winBall
 
     Then("score is Sarah won")
-    game.score() should be ("Sarah won")
+    game.score should be ("Sarah won")
 
   }
 
