94be772d3510a3f725af1ad1c0820f408d64dd90
vfarcic <viktor.farcic@gtech.com>
2014-01-21 13:19:19 +0100

    Word Wrap kata refactor

--- src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
+++ src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
@@ -3,12 +3,9 @@ package com.wordpress.technologyconversations.learning.kata.solutions
 object WordWrap {
 
   def wrap(text: String, maxLength: Int): String = {
-    text.split(" ").foldLeft(Array[String]())(
-      (out, in) => {
-        if (out.size == 0 || (out.last + " " + in).length > maxLength) out :+ (" " + in).trim
-        else out.updated(out.size - 1, out.last + " " + in)
-      }
-    ).mkString("\n")
+    text.split(" ").foldLeft(Array(""))( (out, in) => {
+      if ((out.last + " " + in).trim.length > maxLength) out :+ (" " + in).trim
+      else out.updated(out.size - 1, (out.last + " " + in).trim)
+    }).mkString("\n")
   }
-
 }
