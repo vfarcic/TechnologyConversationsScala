34ab43c693b305014f86e17a7498e718b1cdf23c
vfarcic <viktor.farcic@gtech.com>
2014-01-27 12:11:14 +0100

    Finished Word Wrap kata

--- src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
+++ src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
@@ -2,10 +2,15 @@ package com.wordpress.technologyconversations.learning.kata.solutions
 
 object WordWrap {
 
-  def wrap(text: String, maxLength: Int): String = {
-    text.split(" ").foldLeft(Array(""))( (out, in) => {
-      if ((out.last + " " + in).trim.length > maxLength) out :+ (" " + in).trim
-      else out.updated(out.size - 1, (out.last + " " + in).trim)
-    }).mkString("\n")
+  implicit class StringImprovements(s: String) {
+
+    def wordWrap(maxLength: Int): String = {
+      s.split(" ").foldLeft(Array(""))( (out, in) => {
+        if ((out.last + " " + in).length > maxLength) out :+ in
+        else out.updated(out.size - 1, out.last + " " + in)
+      }).mkString("\n").trim
+    }
+
   }
+
 }
--- src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrapTest.scala
+++ src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrapTest.scala
@@ -1,27 +1,37 @@
 package com.wordpress.technologyconversations.learning.kata.solutions
 
 import org.scalatest.{Matchers, FlatSpec}
+import com.wordpress.technologyconversations.learning.kata.solutions.WordWrap._
 
 /*
 Source: http://codingdojo.org/cgi-bin/wiki.pl?KataWordWrap
 You write a class called Wrapper, that has a single static function named wrap that takes two arguments, a string, and a column number.
 The function returns the string, but with line breaks inserted at just the right places to make sure that no line is longer than the column number.
-You try to break lines at word boundaries.
 
 Like a word processor, break the line by replacing the last space in a line with a newline.
+
+Solution can assume that no word is longer than the maximum number characters in a line.
 */
 class WordWrapTest extends FlatSpec with Matchers {
 
-  "WordWrap#wrap" should "return wrapped text with small sample" in {
+  "WordWrap" should "return empty string is no text is specified" in {
+    "".wordWrap(10) should be ("")
+  }
+
+  it should "work with a single word" in {
+    "kata".wordWrap(4) should be ("kata")
+  }
+
+  it should "return wrapped text with small sample" in {
     val input = "This kata"
     val expected = "This\nkata"
-    WordWrap.wrap(input, 4) should be (expected)
+    input.wordWrap(4) should be (expected)
   }
 
   it should "return wrapped text" in {
     val input = "This kata should be easy unless there are hidden, or not so hidden, obstacles. Let's start!"
     val expected = "This kata\nshould be\neasy unless\nthere are\nhidden, or\nnot so\nhidden,\nobstacles.\nLet's start!"
-    WordWrap.wrap(input, 12) should be (expected)
+    input.wordWrap(12) should be (expected)
   }
 
 }
