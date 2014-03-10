116db18d473326d91379e87ea2e242c29501f7d5
vfarcic <viktor.farcic@gtech.com>
2014-03-10 18:48:21 +0100

    NumbersInWords refactoring

--- src/main/scala/com/technologyconversations/learning/kata/solutions/NumbersInWords.scala
+++ src/main/scala/com/technologyconversations/learning/kata/solutions/NumbersInWords.scala
@@ -3,7 +3,7 @@ package com.technologyconversations.learning.kata.solutions
 class NumbersInWords {
 
   private[solutions] val numbersMap = Map(
-    1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four",
+    0 -> "", 1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four",
     5 -> "five", 6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine",
     10 -> "ten", 11 -> "eleven", 12 -> "twelve", 13 -> "thirteen", 14 -> "fourteen",
     15 -> "fifteen", 16 -> "sixteen", 17 -> "seventeen", 18 -> "eighteen", 19 -> "nineteen",
@@ -14,10 +14,11 @@ class NumbersInWords {
 
   private[solutions] val wordsMap = numbersMap.map(_.swap)
 
-  private[solutions] val multipliers = List(1000000, 1000, 100, 10, 1)
+  private[solutions] val multipliers = List(1000000, 1000, 100, 10, 1, 0)
 
   def numbersToWords(amount: Int): String = {
-    numbersToWords(amount, "").trim
+    if (amount > 0) numbersToWords(amount, "").trim
+    else ""
   }
 
   def wordsToNumbers(amount: String): Int = {
@@ -25,20 +26,18 @@ class NumbersInWords {
   }
 
   private[solutions] def numbersToWords(amount: Int, words: String): String = {
-    for (multiplier <- multipliers) {
-      if (amount >= multiplier) {
-        lazy val times = (amount - (amount % multiplier)) / multiplier
-        lazy val currentAmount = times * multiplier
-        if (amount < 20) {
-          return words + " " + numbersMap(amount)
-        } else if (amount < 100) {
-          return numbersToWords(amount % multiplier, words + " " + numbersMap(currentAmount))
-        } else if (amount >= 1000 && amount < 1000000) {
-          return numbersToWords(amount % multiplier, words + " " + numbersToWords(times) + " " + numbersMap(1000))
-        } else {
-          return numbersToWords(amount % multiplier, words + " " + numbersMap(times) + " " + numbersMap(multiplier))
-        }
-      }
+    val multiplier = multipliers.filter(_ <= amount).head
+    lazy val times = (amount - (amount % multiplier)) / multiplier
+    lazy val currentAmount = times * multiplier
+    lazy val newAmount = amount % multiplier
+    if (amount < 20) {
+      return words + " " + numbersMap(amount)
+    } else if (amount < 100) {
+      return numbersToWords(newAmount, words + " " + numbersMap(currentAmount))
+    } else if (amount >= 1000 && amount < 1000000) {
+      return numbersToWords(newAmount, words + " " + numbersToWords(times) + " " + numbersMap(1000))
+    } else {
+      return numbersToWords(newAmount, words + " " + numbersMap(times) + " " + numbersMap(multiplier))
     }
     words
   }
@@ -50,18 +49,19 @@ class NumbersInWords {
       if (currentWords.isEmpty || !wordsMap.contains(currentWords)) {
         0
       } else {
+        val newAmount = amount.tail
         val currNumber = wordsMap(currentWords)
         if (currNumber < 10) {
-          wordsToNumbers(amount.tail, numbers, currNumber)
+          wordsToNumbers(newAmount, numbers, currNumber)
         } else if (currNumber == 1000) {
           val currTotal = numbers + prevNumber
           val nonMillions = currTotal % 1000000
           val millions = currTotal - nonMillions
-          wordsToNumbers(amount.tail, (nonMillions * currNumber) + millions, 0)
+          wordsToNumbers(newAmount, (nonMillions * currNumber) + millions, 0)
         } else if (prevNumber > 0) {
-          wordsToNumbers(amount.tail, numbers + (currNumber * prevNumber), 0)
+          wordsToNumbers(newAmount, numbers + (currNumber * prevNumber), 0)
         } else {
-          wordsToNumbers(amount.tail, numbers + currNumber, 0)
+          wordsToNumbers(newAmount, numbers + currNumber, 0)
         }
       }
     }
