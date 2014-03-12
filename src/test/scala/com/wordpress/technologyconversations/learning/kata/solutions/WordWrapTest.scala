98df8c3b9745f0d92ab4e6c94648541ae6b1e25d
vfarcic <viktor.farcic@gtech.com>
2014-03-12 09:52:48 +0100

    Fixed WordWrap kata bug

--- src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrapTest.scala
+++ src/test/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrapTest.scala
@@ -35,8 +35,8 @@ class WordWrapTest extends FlatSpec with Matchers {
   }
 
   it should "return the same text if max length is the same as the length of the text" in {
-    val input = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    val expected = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    val input = "Lorem ipsum dolor sit amet."
+    val expected = "Lorem ipsum dolor sit amet."
     input.wordWrap(input.length) should be (expected)
   }
 
