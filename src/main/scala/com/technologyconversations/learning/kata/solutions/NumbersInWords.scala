0be8e3761dccb5c0a2c10e2792842656f3dd3825
vfarcic <viktor.farcic@gtech.com>
2014-03-12 10:33:59 +0100

    NumbersInWords refactoring

--- src/main/scala/com/technologyconversations/learning/kata/solutions/NumbersInWords.scala
+++ src/main/scala/com/technologyconversations/learning/kata/solutions/NumbersInWords.scala
@@ -28,42 +28,34 @@ class NumbersInWords {
   private[solutions] def numbersToWords(amount: Int, words: String): String = {
     val multiplier = multipliers.filter(_ <= amount).head
     lazy val times = (amount - (amount % multiplier)) / multiplier
-    lazy val currentAmount = times * multiplier
     lazy val newAmount = amount % multiplier
     if (amount < 20) {
-      return words + " " + numbersMap(amount)
+      words + " " + numbersMap(amount)
     } else if (amount < 100) {
-      return numbersToWords(newAmount, words + " " + numbersMap(currentAmount))
+      numbersToWords(newAmount, words + " " + numbersMap(times * multiplier))
     } else if (amount >= 1000 && amount < 1000000) {
-      return numbersToWords(newAmount, words + " " + numbersToWords(times) + " " + numbersMap(1000))
+      numbersToWords(newAmount, words + " " + numbersToWords(times) + " " + numbersMap(1000))
     } else {
-      return numbersToWords(newAmount, words + " " + numbersMap(times) + " " + numbersMap(multiplier))
+      numbersToWords(newAmount, words + " " + numbersMap(times) + " " + numbersMap(multiplier))
     }
-    words
   }
 
   private[solutions] def wordsToNumbers(amount: List[String], numbers: Int, prevNumber: Int): Int = {
-    if (amount == Nil) numbers + prevNumber
-    else {
-      val currentWords = amount.head.trim
-      if (currentWords.isEmpty || !wordsMap.contains(currentWords)) {
-        0
-      } else {
-        val newAmount = amount.tail
-        val currNumber = wordsMap(currentWords)
-        if (currNumber < 10) {
-          wordsToNumbers(newAmount, numbers, currNumber)
-        } else if (currNumber == 1000) {
-          val currTotal = numbers + prevNumber
-          val nonMillions = currTotal % 1000000
-          val millions = currTotal - nonMillions
-          wordsToNumbers(newAmount, (nonMillions * currNumber) + millions, 0)
-        } else if (prevNumber > 0) {
-          wordsToNumbers(newAmount, numbers + (currNumber * prevNumber), 0)
-        } else {
-          wordsToNumbers(newAmount, numbers + currNumber, 0)
-        }
-      }
+    if (amount == Nil) return numbers + prevNumber
+    val currentWords = amount.head.trim
+    if (currentWords.isEmpty || !wordsMap.contains(currentWords)) return 0
+    val currNumber = wordsMap(currentWords)
+    if (currNumber < 10) {
+      wordsToNumbers(amount.tail, numbers, currNumber)
+    } else if (currNumber == 1000) {
+      val currTotal = numbers + prevNumber
+      val nonMillions = currTotal % 1000000
+      val millions = currTotal - nonMillions
+      wordsToNumbers(amount.tail, (nonMillions * currNumber) + millions, 0)
+    } else if (prevNumber > 0) {
+      wordsToNumbers(amount.tail, numbers + (currNumber * prevNumber), 0)
+    } else {
+      wordsToNumbers(amount.tail, numbers + currNumber, 0)
     }
   }
 
