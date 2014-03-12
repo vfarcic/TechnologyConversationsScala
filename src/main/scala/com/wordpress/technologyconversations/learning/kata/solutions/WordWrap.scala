4d7ffefe6672c5c9f0f2f8e339ea441443a1059b
vfarcic <viktor.farcic@gtech.com>
2014-03-12 09:51:33 +0100

    Fixed WordWrap kata bug

--- src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
+++ src/main/scala/com/wordpress/technologyconversations/learning/kata/solutions/WordWrap.scala
@@ -6,7 +6,7 @@ object WordWrap {
 
     def wordWrap(maxLength: Int): String = {
       s.split(" ").foldLeft(Array(""))( (out, in) => {
-        if ((out.last + " " + in).length > maxLength) out :+ in
+        if ((out.last + " " + in).trim.length > maxLength) out :+ in
         else out.updated(out.size - 1, out.last + " " + in)
       }).mkString("\n").trim
     }
